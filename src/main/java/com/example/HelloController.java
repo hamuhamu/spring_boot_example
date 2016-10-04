e3acc8cfecbbe37e0c80070ac3e2414e4397b6c7
hamuhamu <hrdng4922000@yahoo.co.jp>
2016-10-04 22:53:50 +0900

    GetMappingに変更

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,13 +1,13 @@
 package com.example;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // このアノテーションを付けることで、コントローラクラスになる
 @RestController
 public class HelloController {
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String sayHello() {
 		return "Hello world";
 	}
