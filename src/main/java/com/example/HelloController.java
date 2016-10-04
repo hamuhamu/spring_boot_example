603f01ea9634e0ed3157ac567c52d8c09ec0eb24
hamuhamu <hrdng4922000@yahoo.co.jp>
2016-10-04 22:51:26 +0900

    コメント追加

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -3,8 +3,10 @@ package com.example;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// このアノテーションを付けることで、コントローラクラスになる
 @RestController
 public class HelloController {
+
 	@RequestMapping("/")
 	public String sayHello() {
 		return "Hello world";
