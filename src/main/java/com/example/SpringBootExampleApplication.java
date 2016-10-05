7e7f44a457de1fe5635ccae480368ab627787100
hamuhamu <hrdng4922000@yahoo.co.jp>
2016-10-06 00:59:09 +0900

    DIを使えるようにした

--- src/main/java/com/example/SpringBootExampleApplication.java
+++ src/main/java/com/example/SpringBootExampleApplication.java
@@ -1,12 +1,20 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.ApplicationContext;
+import com.example.di.CalculatorInterface;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@Import(DiConfig.class)
 public class SpringBootExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootExampleApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SpringBootExampleApplication.class, args);
+		CalculatorInterface c = context.getBean(CalculatorInterface.class);
+		System.out.println(c.calc(1, 2));
 	}
 }
